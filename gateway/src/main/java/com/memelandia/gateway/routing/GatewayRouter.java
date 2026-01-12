07327559f68135d973836c3706d3b72f11c02c48
k-lila <krishnalilapalandi@gmail.com>
2026-01-12 02:33:58 -0300

    Gateway

---
+++ gateway/src/main/java/com/memelandia/gateway/routing/GatewayRouter.java
@@ -0,0 +1,21 @@
+package com.memelandia.gateway.routing;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRouter {
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route(p -> p
+				.path("/get")
+				.filters(f -> f.addRequestHeader("Hello", "World"))
+				.uri("http://httpbin.org:80"))
+		.build();
+	}
+
+}
