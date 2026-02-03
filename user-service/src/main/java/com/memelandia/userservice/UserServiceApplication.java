779310ca43476e8203acafeb00bf4f71bb2a3393
k-lila <krishnalilapalandi@gmail.com>
2026-02-03 19:38:44 -0300

    Zipkin no ambiente docker. Docker-compose para conexão com atlas ou banco de dados local

--- user-service/src/main/java/com/memelandia/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/memelandia/userservice/UserServiceApplication.java
@@ -2,8 +2,11 @@ package com.memelandia.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.mongodb.autoconfigure.health.MongoHealthContributorAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+	exclude = MongoHealthContributorAutoConfiguration.class
+)
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
